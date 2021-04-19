d1d5f8b9757e32a36babf3332ee62e1cdd255315
Kellie <littlekel03@gmail.com>
2021-04-19 17:03:49 +0800

    tried to fix errors

--- assests/script.js
+++ assests/script.js
@@ -68,7 +68,6 @@ function displayQuote() {
         })
 };
 
-
 function signUpUser(event) {
     event.preventDefault();
     var userName = userNameInput.value;
@@ -246,13 +245,3 @@ submitButton.onclick = function (event) {
         console.log("please make a submission")
     }
 }
-
-
-var expenseName = localStorage.getItem("expenseName")
-
-
-var expenseAmount = localStorage.getItem("expenseAmount")
-
-
-var expenseCatagory = localStorage.getItem("expenseCatagory")
-
