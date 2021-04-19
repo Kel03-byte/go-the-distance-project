20a97c9ab8fa0a91d68c6304e3611b38cacd05fe
Kellie <littlekel03@gmail.com>
2021-04-19 11:55:52 +0800

    styled and coded holiday input page

--- assests/script.js
+++ assests/script.js
@@ -1,14 +1,74 @@
-//Variables for "Sign Up" on website.
 var signUpButton = document.getElementById('sign-up-button')
+var signInOneButton = document.getElementById('sign-in-button');
+var signInTwoButton = document.getElementById('sign-in-button-two');
+var holidaySubmitButton = document.getElementById("holiday-submit-button")
+var signOutOneButton = document.getElementById('sign-out-button');
+var signOutTwoButton = document.getElementById('sign-out-button-two');
+var confirmButtonEl = document.createElement("button")
+
+var signUpContainer = document.getElementById('sign-up-container')
+var holidayInputContainer = document.getElementById('holiday-input-container')
+var signInContainer = document.getElementById('sign-in-container');
+var expenseInputContainer = document.getElementById('expense-input-container')
+
 var userNameInput = document.querySelector('#username');
 var userEmailInput = document.querySelector('#email');
 var userPasswordInput = document.querySelector('#password');
-var signInButton = document.querySelector('#sign-in-button');
-var signUpContainer = document.getElementById('sign-up-container')
 
-//"Sign Up" event listener which if successful stores the users email, password and name in local storage
-// and then the user is taken to the input page to input their holiday budget info.
-signUpButton.addEventListener('click', function (event) {
+var userInputEmail = document.getElementById("user-input-email").value
+var userInputPassword = document.getElementById("user-input-password").value
+var email = localStorage.getItem("Email", email);
+var password = localStorage.getItem("Password", password)
+
+var quoteButton = document.getElementById("quote-button")
+var quoteText = document.getElementById("quote")
+
+
+displayQuote()
+
+//Function to display Sign In Container which the user sings back into the site
+//and can add/edit holiday expenses
+function displaySignInContainer() {
+    signUpContainer.style.display = "none"
+    signInContainer.style.display = "block"
+    holidayInputContainer.style.display = "none"
+    expenseInputContainer.style.display = "none"
+}
+
+//Function to display the Holiday Input Container where the user enters details about their travel
+function displayHolidayInputContainer() {
+    signUpContainer.style.display = "none"
+    signInContainer.style.display = "none"
+    holidayInputContainer.style.display = "block"
+    expenseInputContainer.style.display = "none"
+
+}
+
+//Function to display the Expense Container where the user inputs their expenses
+//and updates the progress bar
+function displayExpenseInputContainer() {
+    signUpContainer.style.display = "none"
+    signInContainer.style.display = "none"
+    holidayInputContainer.style.display = "none"
+    expenseInputContainer.style.display = "block"
+}
+
+//Function to fetch and display a random quote
+function displayQuote() {
+    fetch("https://favqs.com/api/qotd")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var quote = data.quote.body
+            var author = data.quote.author
+            quoteText.innerHTML = "<q>" + quote + "</q>" + "<p>" + author + "<p>"
+        })
+};
+
+
+
+function signUpUser(event) {
     event.preventDefault();
     var storedEmail = localStorage.getItem("Email", email)
     var name = userNameInput.value;
@@ -17,15 +77,19 @@ signUpButton.addEventListener('click', function (event) {
 
     //If statements to alert the user to input info or that they have already signed up
     if (!name) {
+        displayModal()
         return
     }
     if (!email) {
+        displayModal()
         return
     }
     if (storedEmail == email) {
-        return alert("you already have singed up before!")
+        displayModal()
+        return
     }
-    if (!password) {
+    if (!password || password.length <= 7) {
+        displayModal()
         return
     }
     else {
@@ -35,34 +99,11 @@ signUpButton.addEventListener('click', function (event) {
         displayHolidayInputContainer()
     }
     return
-});
-
-//Variables for "Sign In" page if the user has already signed up before.
-var signInContainer = document.getElementById('sign-in-container');
-var signInTwoButton = document.getElementById('sign-in-button-two');
-
-//"Sign In" event listener which displays the "Sign In" container and allows the user
-//to sign in without signing up again.
-signInButton.addEventListener("click", displaySignInContainer);
-
-//Funtion to display the "Sign In" container and remove the "Sign Up" container.
-function displaySignInContainer() {
-    signUpContainer.remove()
-    signInContainer.style.display = "block";
-}
-
-//Event listener to submit the user's "Sign In" information and display the "Holiday Budget Input" container.
-signInTwoButton.addEventListener("click", getUserInformation)
+};
 
 //Function to check user's info and dipslay the "Holiday Budget Input" container.
 function getUserInformation() {
     displaySignInContainer()
-
-    //local variables to pull locally stored info and user's input
-    var userInputEmail = document.getElementById("user-input-email").value
-    var email = localStorage.getItem("Email", email);
-    var userInputPassword = document.getElementById("user-input-password").value
-    var password = localStorage.getItem("Password", password)
 
     //IF statements that will occur when checking the locally stored info against user's input
     if (userInputEmail !== email && userInputPassword !== password) {
@@ -73,71 +114,68 @@ function getUserInformation() {
         return alert("password is the same, but email doesn't match")
     } else (userInputEmail == email && userInputPassword == password)
     displayHolidayInputContainer()
-}
-
-//Removes "Sign Up" and "Sign In" container and displays the "Holiday Budget Input" container
-var holidayInputContainer = document.getElementById("holiday-input-container")
-function displayHolidayInputContainer() {
-    signUpContainer.remove()
-    signInContainer.remove()
-    holidayInputContainer.style.display = "block"
-}
-
-//Gloabl variables for user's holiday budget input
-var holidaySubmitButton = document.getElementById("holiday-submit-button")
-var holidayResults = document.getElementById("holiday-budget-result")
-var confirmButtonEl = document.createElement("button")
-var resubmitButtonEl = document.createElement("button")
+};
 
 //Event listener to collect user's input
 holidaySubmitButton.addEventListener("click", function (event) {
-
-    //local variables for destination, dates and budgeted amount
     var userDestinationList = document.getElementById("destination-list");
-    var budgetTotal = document.getElementById("holiday-budget-total").value;
+    var holidayResults = document.getElementById("holiday-budget-result")
     var userDestination = userDestinationList.options[userDestinationList.selectedIndex].text;
+    var budgetTotal = document.getElementById("holiday-budget-total").value;
     var dateOne = document.getElementById('start-date').value
     var dateTwo = document.getElementById('end-date').value
     var startDate = moment(dateOne).format("DD/MM/YYYY")
     var endDate = moment(dateTwo).format("DD/MM/YYYY")
 
     //IF statements for when the user does/doesn't enter their input
-    if (userDestination == "---Destinations---") {
-        displayModal();
-        return
-    } else if (!budgetTotal) {
-        displayModal();
-        return
-    } else if (!dateOne) {
-        displayModal()
-        return
-    } else if (!dateTwo) {
-        displayModal();
-        return
-    } else {
-        //If user enters all info then a message is displayed to confirm their input or to resubmit
-        event.preventDefault();
-        holidayResults.textContent = 'You have selected to holiday at ' + userDestination +
-            ', from: ' + startDate +
-            ', till: ' + endDate +
-            ' with $' + budgetTotal + ' to spend while on holiday, is that correct?'
-        holidayResults.append();
-        confirmButtonEl.textContent = "Confirm"
-        resubmitButtonEl.textContent = "Resubmit"
-        holidayResults.appendChild(confirmButtonEl)
-        holidayResults.appendChild(resubmitButtonEl)
-    };
-    return;
+        if (userDestination == "---Destinations---") {
+            displayModal();
+            return
+        } else if (!budgetTotal) {
+            displayModal();
+            return
+        } else if (!dateOne) {
+            displayModal()
+            return
+        } else if (!dateTwo) {
+            displayModal();
+            return
+        } else {
+            //If user enters all info then a message is displayed to confirm their input or to resubmit
+    event.preventDefault();
+    holidaySubmitButton.style.display = "none"
+    holidayResults.textContent = 'You have selected to holiday at ' + userDestination +
+        ', from: ' + startDate +
+        ', till: ' + endDate +
+        ' with $' + budgetTotal + ' to spend while on holiday, is that correct?'
+    holidayResults.append();
+    confirmButtonEl.textContent = "Confirm"
+    confirmButtonEl.classList = "button is-info confirm-button"
+    holidayResults.appendChild(confirmButtonEl)
+}});
+
+//Event listeners to sign out, confirm holiday details, to go to the sign in again page
+// and when signing in again from the sign in page instead of the sign up page
+signUpButton.addEventListener('click', displayHolidayInputContainer)
+signOutOneButton.addEventListener('click', function () { location.reload() });
+signOutTwoButton.addEventListener('click', function () { location.reload() });
+confirmButtonEl.addEventListener('click', displayExpenseInputContainer);
+signInOneButton.addEventListener("click", displaySignInContainer);
+signInTwoButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    displayExpenseInputContainer()
 });
 
-resubmitButtonEl.addEventListener("click", function () { location.reload() });
-
-
-//Function to display Modals when an event happens
-var modal = document.getElementById("page-modal");
+//Function to display Modals when a User doesn't enter their information
+var modal = document.getElementById("pageModal");
 var span = document.getElementsByClassName("close")[0];
+
 function displayModal() {
     modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
 window.onclick = function (event) {
@@ -146,7 +184,59 @@ window.onclick = function (event) {
     }
 }
 
-span.onclick = function () {
-    modal.style.display = "none";
+var expenseModal = document.getElementById("expenseModal");
+var addExpenseButton = document.getElementById("addExpenseButton");
+var closeButton = document.getElementsByClassName("closeButton")[0];
+
+addExpenseButton.onclick = function () {
+    expenseModal.style.display = "block";
 }
+
+closeButton.onclick = function () {
+    expenseModal.style.display = "none";
+}
+
+var submitButton = document.getElementById("submitButton")
+
+submitButton.onclick = function (event) {
+    event.preventDefault()
+
+    var expenseName = document.getElementById("expenseName").value;
+    localStorage.setItem("expenseName", JSON.stringify(expenseName))
+
+    var expenseAmount = document.getElementById("expenseAmount").value;
+    localStorage.setItem("expenseAmount", JSON.stringify(expenseAmount))
+
+    var expenseCatagory = document.getElementById("expenseCatagory").value;
+    localStorage.setItem("expenseCatagory", JSON.stringify(expenseCatagory))
+
+    if (expenseName == "") {
+        displayModal()
+        console.log("please make a submission")
+    }
+
+    if (expenseAmount == "") {
+        displayModal()
+        console.log("please make a submission")
+    }
+
+    if (expenseAmount != Number) {
+        displayModal()
+        console.log("please submit a number")
+    }
+
+    if (expenseCatagory == "") {
+        displayModal()
+        console.log("please make a submission")
+    }
+}
+
+
+var expenseName = localStorage.getItem("expenseName")
+
+
+var expenseAmount = localStorage.getItem("expenseAmount")
+
+
+var expenseCatagory = localStorage.getItem("expenseCatagory")
 
